e2408602b49e85f4e40f3bf2568017ba13cc5f53
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-06-26 23:36:33 +0800

    get config name

---
+++ Rino/ConfigName.c
@@ -0,0 +1,9 @@
+// 获取配置文件名称
+static char* GetConfigFileName() {
+	return "rino_config.ini";
+}
+
+// 获取无后缀名的配置文件名称
+static char* GetConfigFileNameNoSuffix() {
+	return "rino_config";
+}
---
+++ Rino/Strings.h
@@ -0,0 +1,4 @@
+#pragma once
+
+static char* GetConfigFileName(); // 获取配置文件名称
+static char* GetConfigFileNameNoSuffix(); // 获取无后缀名的配置文件名称
