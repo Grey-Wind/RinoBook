eafd7f07047da1881a1354ba56b5d82574bcd903
Grey Wind <wjunqin7@gmail.com>
2025-04-12 01:47:15 +0800

    add the default method
    unknow command

---
+++ src/CLI/Rino.CLI.Help/Error.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Rino.CLI.Help
+{
+    /// <summary>
+    /// 输出错误的类
+    /// </summary>
+    public class Error
+    {
+        public static void UnknowCommand(string command)
+        {
+            Console.Write("Unknow command ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(command);
+            Console.ResetColor();
+        }
+    }
+}
--- src/CLI/Rino.CLI/Program.cs
+++ src/CLI/Rino.CLI/Program.cs
@@ -36,6 +36,7 @@ namespace Rino.CLI
                             case "build":
                                 break;
                             default:
+                                Error.UnknowCommand(inputCommand);
                                 break;
                         }
 
